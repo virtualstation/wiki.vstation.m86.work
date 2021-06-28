132c37c93741adcece185de04661daa48d86cae7
Minato86 <doronomori660@outlook.jp>
2021-06-29 03:22:33 +0900

    wip 基本の構成 #1

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Vuepress Docs Boilerplate',
+  title: 'Virtual Station Wiki✨',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
@@ -27,11 +27,12 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: '',
-    editLinks: false,
-    docsDir: '',
-    editLinkText: '',
-    lastUpdated: false,
+    repo: 'virtualstation/wiki.vstation.m86.work',
+    editLinks: true,
+    docsDir: 'src',
+    docsBranch: 'develop',
+    editLinkText: 'このページをGithubで編集',
+    lastUpdated: "最終更新",
     nav: [
       {
         text: 'Guide',
